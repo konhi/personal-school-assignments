755b18ed2025c4e93edeed7b58b0bf51f52830be
Jan Szymański <hello.konhi@gmail.com>
2022-05-11 12:59:50 +0200

    export

---
+++ 2021/01 Databases/02 Practice/02 PHP/02 Connecting/p05.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 11 Maj 2022, 12:59
+-- Wersja serwera: 10.4.19-MariaDB
+-- Wersja PHP: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Baza danych: `p05`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `historia`
+--
+
+CREATE TABLE `historia` (
+  `id` int(11) NOT NULL,
+  `id_ucznia` int(11) NOT NULL,
+  `id_ksiazki` int(11) NOT NULL,
+  `data_wypozyczenia` date NOT NULL,
+  `data_zwrotu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ksiazki`
+--
+
+CREATE TABLE `ksiazki` (
+  `id` int(11) NOT NULL,
+  `tytul` varchar(255) NOT NULL,
+  `autor` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `ksiazki`
+--
+
+INSERT INTO `ksiazki` (`id`, `tytul`, `autor`) VALUES
+(1, 'Tytuł książki', 'Autor książki');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uczniowie`
+--
+
+CREATE TABLE `uczniowie` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(255) NOT NULL,
+  `nazwisko` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `uczniowie`
+--
+
+INSERT INTO `uczniowie` (`id`, `imie`, `nazwisko`) VALUES
+(1, 'Imię ucznia', 'Nazwisko ucznia');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wypozyczenia`
+--
+
+CREATE TABLE `wypozyczenia` (
+  `id` int(11) NOT NULL,
+  `id_ksiazki` int(11) NOT NULL,
+  `id_ucznia` int(11) NOT NULL,
+  `data_wypozyczenia` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `wypozyczenia`
+--
+
+INSERT INTO `wypozyczenia` (`id`, `id_ksiazki`, `id_ucznia`, `data_wypozyczenia`) VALUES
+(3, 1, 1, '2022-05-11');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `historia`
+--
+ALTER TABLE `historia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `uczniowie`
+--
+ALTER TABLE `uczniowie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `historia`
+--
+ALTER TABLE `historia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `uczniowie`
+--
+ALTER TABLE `uczniowie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
